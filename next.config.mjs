0a8566290c5d247595b4c685f521b21d3f136d6d
Verissimo Ribeiro <vribeiro@landytech.com>
2024-10-13 09:29:51 +0100

    fix(build): disabling lint https://github.com/vercel/next.js/issues/64409

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true, // Disable image optimization for static export
-    },
+  output: "export",
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
